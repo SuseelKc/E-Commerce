4b6f96551b160268d29cf7bac0b3e6b5999f4b81
Sushil Khatri <139630872+SuseelKc@users.noreply.github.com>
2023-09-12 16:27:06 +0545

    logout/login

--- Ecommerce/routes/web.php
+++ Ecommerce/routes/web.php
@@ -1,7 +1,8 @@
-<?php
+ <?php
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\Admin\CategoryController;
 use App\Http\Controllers\Admin\DashboardController;
 
 /*
@@ -23,6 +24,8 @@ Auth::routes();
 
 Route::get('/home', [HomeController::class, 'index'])->name('home');
 
-Route::prefix('admin')->group(function (){
+Route::prefix('admin')->middleware(['auth','isAdmin'])->group(function (){
     Route:: get ('dashboard',[DashboardController::class, 'index']);
+    // category
+    Route:: get ('category',[CategoryController::class, 'index']);
 });
